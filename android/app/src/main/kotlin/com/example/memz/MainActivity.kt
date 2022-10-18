0199f18ac4f551035dd9ff784e65681c1f914b12
Elizabeth Ha <liz@atomism.co>
2022-10-17 17:01:28 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/memz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.memz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:memz/screens/authentication/email_password/sign_in_screen.dart';
+
+import 'screens/main_screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterFire Samples',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'OpenSans',
+      ),
+      home: SignInScreen(),
+      // home: const HomeScreen(),
+    );
+  }
+}
