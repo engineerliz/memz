e592b3594ed37cd10d0e2cccb8f6d1cd6bd5f37f
Elizabeth Ha <liz@atomism.co>
2022-11-21 22:50:24 -0800

    Add tap on profile grid posts

--- lib/components/pin/GridSinglePin.dart
+++ lib/components/pin/GridSinglePin.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:memz/api/pins/PinModel.dart';
 import 'package:memz/components/map/Map.dart';
+import 'package:memz/features/pinPost/PinPostView.dart';
 import 'package:memz/styles/colors.dart';
 
 import '../shimmer/ShimmerBox.dart';
@@ -19,23 +20,32 @@ class GridSinglePin extends StatelessWidget {
     double height = MediaQuery.of(context).size.width * .65;
     double width = MediaQuery.of(context).size.width / 2;
     if (pinData.imgUrls?.isNotEmpty == true) {
-      return Container(
-        height: MediaQuery.of(context).size.width * .65,
-        width: MediaQuery.of(context).size.width / 2,
-        decoration: BoxDecoration(
-          border: Border.all(color: MColors.background),
-          ),
-          child: CachedNetworkImage(
-            height: height,
-            width: width,
-            fit: BoxFit.cover,
-            imageUrl: pinData.imgUrls!.first,
-            placeholder: (context, url) => ShimmerBox(
-              height: height,
-              radius: width,
-            ),
-          )
-      );
+      return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => PinPostView(
+                  pin: pinData,
+                ),
+              ),
+            );
+          },
+          child: Container(
+              height: MediaQuery.of(context).size.width * .65,
+              width: MediaQuery.of(context).size.width / 2,
+              decoration: BoxDecoration(
+                border: Border.all(color: MColors.background),
+              ),
+              child: CachedNetworkImage(
+                height: height,
+                width: width,
+                fit: BoxFit.cover,
+                imageUrl: pinData.imgUrls!.first,
+                placeholder: (context, url) => ShimmerBox(
+                  height: height,
+                  radius: width,
+                ),
+              )));
     }
     return Container(
       height: MediaQuery.of(context).size.width * .65,
--- lib/features/pinPost/PinPostView.dart
+++ lib/features/pinPost/PinPostView.dart
@@ -39,67 +39,7 @@ class _PinPostViewState extends State<PinPostView> {
     // final double height = MediaQuery.of(context).size.height;
     return CommonScaffold(
       title: '',
-      body: Column(children: [
-        CarouselSlider(
-          options: CarouselOptions(
-              height: 500,
-              viewportFraction: 1.0,
-              enableInfiniteScroll: false,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _current = index;
-                });
-              }),
-          items: [
-            Container(
-              clipBehavior: Clip.hardEdge,
-              // width: MediaQuery.of(context).size.width,
-              // height: 500,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(widget.pin.imgUrls!.first),
-                ),
-              ),
-            ),
-            Container(
-              height: 200,
-              // width: 150,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Map(
-                location: widget.pin.location,
-                zoom: 11,
-                // onTap: (latlng) {
-                //   onPostTap();
-                // },
-              ),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [1, 2].asMap().entries.map((entry) {
-            return GestureDetector(
-              onTap: () => _controller.animateToPage(entry.key),
-              child: Container(
-                width: 12.0,
-                height: 12.0,
-                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: (Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black)
-                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-              ),
-            );
-          }).toList(),
-        ),
-      ]),
+      body: PinPost(pin: widget.pin),
     );
   }
 }
