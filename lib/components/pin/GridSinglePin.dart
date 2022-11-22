e66745e5f55a6320f29fa872d71b505f15bbd7fe
Elizabeth Ha <liz@atomism.co>
2022-11-21 22:23:47 -0800

    Cache images on profile

--- lib/components/pin/GridSinglePin.dart
+++ lib/components/pin/GridSinglePin.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:memz/api/pins/PinModel.dart';
 import 'package:memz/components/map/Map.dart';
 import 'package:memz/styles/colors.dart';
+
+import '../shimmer/ShimmerBox.dart';
 
 class GridSinglePin extends StatelessWidget {
   final PinModel pinData;
@@ -13,17 +16,25 @@ class GridSinglePin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.width * .65;
+    double width = MediaQuery.of(context).size.width / 2;
     if (pinData.imgUrls?.isNotEmpty == true) {
       return Container(
         height: MediaQuery.of(context).size.width * .65,
         width: MediaQuery.of(context).size.width / 2,
         decoration: BoxDecoration(
           border: Border.all(color: MColors.background),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(pinData.imgUrls!.first),
           ),
-        ),
+          child: CachedNetworkImage(
+            height: height,
+            width: width,
+            fit: BoxFit.cover,
+            imageUrl: pinData.imgUrls!.first,
+            placeholder: (context, url) => ShimmerBox(
+              height: height,
+              radius: width,
+            ),
+          )
       );
     }
     return Container(
